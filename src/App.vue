797606a4ff24fb196154a3bb01b05e94826c16c6
elnish <a.shobotova@hotmail.com>
2019-06-07 12:09:22 +0300

    add project

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <Header/>
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header.vue'
+import AddTodo from './components/AddTodo.vue'
+import Todos from './components/Todos.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    AddTodo,
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    }
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  #app {
+    width: 800px;
+    margin: 0 auto;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: rgb(3, 59, 11);
+    color: #fff;
+    font-size: 16px;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: rgb(10, 92, 65);
+  }
+</style>
---
+++ src/components/Todoitem.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="makeComplete">
+            {{ todo.title }}
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+            </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Todoitem',
+    props: ["todo"],
+    methods: {
+        makeComplete() {
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-item {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+    .is-complete {
+        text-decoration: line-through;
+    }
+    .del {
+        background: #ff0000;
+        color: #fff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+        opacity: .1;
+        transition: opacity .7s ease-in-out;
+    }
+    .del:hover {
+        opacity: 1;
+    }
+</style>
+
+
